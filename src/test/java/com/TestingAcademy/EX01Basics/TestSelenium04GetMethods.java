f949e9d4da6fcf1109208486fb4064f5809dd980
Admin <atifmohd635@gmail.com>
2026-01-11 09:39:42 +0530

    SeleniumLearnATB14X

--- src/test/java/com/TestingAcademy/EX01Basics/TestSelenium04GetMethods.java
+++ src/test/java/com/TestingAcademy/EX01Basics/TestSelenium04GetMethods.java
@@ -1,13 +1,14 @@
 package com.TestingAcademy.EX01Basics;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class TestSelenium04GetMethods {
     public static void main(String[] args) {
-        WebDriver driver=new FirefoxDriver();
-        driver.get("https://app.vwo.com");
-        System.out.println(driver.getCurrentUrl());
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://www.google.com");
         System.out.println(driver.getTitle());
+        System.out.println(driver.getCurrentUrl());
     }
 }
